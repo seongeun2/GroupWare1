38d7e6515b5fc431a84d791b897646ee24988dba
se <se@my-pc>
2018-04-11 16:50:13 +0900

    04.11 board 반장님 소스 통합

--- src/controller/ComController.java
+++ src/controller/ComController.java
@@ -23,77 +23,68 @@ public class ComController {
 			return "index";
 		}
 		
-		//===============> 장효추가 start
-		String boardid = "1";
-		String pageNum ="1";
-		BoardDBMybatis dbPro = BoardDBMybatis.getInstance();
-		@ModelAttribute
-		public void addAttributes (String boardid, String pageNum) {
-			if (pageNum != null && pageNum != "") this.pageNum = pageNum;
-		}
-	//===============> 장효추가 end
+		 //===============> 장효추가 start
+	      String boardid = "1";
+	      String pageNum ="1";
+	      BoardDBMybatis dbPro = BoardDBMybatis.getInstance();
+	      @ModelAttribute
+	      public void addAttributes (String boardid, String pageNum) {
+	         if (pageNum != null && pageNum != "") this.pageNum = pageNum;
+	      }
+	   //===============> 장효추가 end
 
-		
-		@RequestMapping("/main")
-		public String list1(Model model, HttpSession session) throws Exception {
-			
-			String name = (String) session.getAttribute("name");
-			String id = (String) session.getAttribute("id");
-			model.addAttribute("name", name);
-			//추가
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
-			//SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-			int pageSize = 7;
-			int currentPage = Integer.parseInt(pageNum);
-			int startRow = (currentPage - 1) * pageSize + 1;
-			int endRow = currentPage * pageSize;
-			int count = 0;
-			int number = 0;
-			
-			//
-			List articleList1 = null;
-			count = dbPro.getArticleCount1();  
-			if (count > 0) {
-					articleList1 = dbPro.getArticles1(startRow, endRow);	
-				}
-			number = count - (currentPage - 1) * pageSize;
-			int bottomLine = 3;
-			int pageCount = count / pageSize + (count % pageSize == 0 ? 0 : 1);
-			int startPage = 1 + (currentPage - 1) / bottomLine * bottomLine;
-			int endPage = startPage + bottomLine - 1;
-			if (endPage > pageCount) endPage = pageCount;
-			
-			//
-			List articleList2 = null;
-			count = dbPro.getArticleCount2();  
-			if (count > 0) {
-					articleList2 = dbPro.getArticles2(startRow, endRow);	
-				}
-		
-			model.addAttribute("boardid", null);
-			model.addAttribute("count", count);
-			model.addAttribute("articleList", articleList1);
-			model.addAttribute("articleList1", articleList2);
-			model.addAttribute("currentPage", currentPage);
-			model.addAttribute("startPage", startPage);
-			model.addAttribute("bottomLine", bottomLine);
-			model.addAttribute("endPage", endPage);
-			model.addAttribute("number", number);
-		
-		/* 취소-----(4/8)
-	    //성희 추가 top.jspf에 왼쪽 버튼 클릭시 관리자용 메인으로 이동.
-		if(id.equals("admin")) {
-			return "common/admin";
-		}
-		
-		//성희 추가 끝
-		*/ 
-		
-		//수정
-			
-			return "board/mainBoard";
-		}
+	      
+	      @RequestMapping("/main")
+	      public String list1(Model model, HttpSession session) throws Exception {
+	         
+	         String name = (String) session.getAttribute("name");
+	         
+	         model.addAttribute("name", name);
+	         //추가
+	         
+	         SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
+	         //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	         int pageSize = 7;
+	         int currentPage = Integer.parseInt(pageNum);
+	         int startRow = (currentPage - 1) * pageSize + 1;
+	         int endRow = currentPage * pageSize;
+	         int count = 0;
+	         int number = 0;
+	         
+	         //
+	         List articleList1 = null;
+	         count = dbPro.getArticleCount1();  
+	         if (count > 0) {
+	               articleList1 = dbPro.getArticles1(startRow, endRow);   
+	            }
+	         number = count - (currentPage - 1) * pageSize;
+	         int bottomLine = 3;
+	         int pageCount = count / pageSize + (count % pageSize == 0 ? 0 : 1);
+	         int startPage = 1 + (currentPage - 1) / bottomLine * bottomLine;
+	         int endPage = startPage + bottomLine - 1;
+	         if (endPage > pageCount) endPage = pageCount;
+	         
+	         //
+	         List articleList2 = null;
+	         count = dbPro.getArticleCount2();  
+	         if (count > 0) {
+	               articleList2 = dbPro.getArticles2(startRow, endRow);   
+	            }
+	      
+	         model.addAttribute("boardid", null);
+	         model.addAttribute("count", count);
+	         model.addAttribute("articleList1", articleList1);
+	         model.addAttribute("articleList2", articleList2);
+	         model.addAttribute("currentPage", currentPage);
+	         model.addAttribute("startPage", startPage);
+	         model.addAttribute("bottomLine", bottomLine);
+	         model.addAttribute("endPage", endPage);
+	         model.addAttribute("number", number);
+	      //수정
+	         
+	         return "board/mainBoard";
+	      }
+
 		
 		//로그아웃
 		@RequestMapping("/logout")
